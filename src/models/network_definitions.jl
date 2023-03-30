cd5c036b1af6c6a865feda65d02e6565edc50e23
Romanos Voulgarakis <voulgarakisromanos@gmail.com>
2023-03-30 19:03:31 +0300

    Added arg parsing

--- src/models/network_definitions.jl
+++ src/models/network_definitions.jl
@@ -1,52 +1,56 @@
 using Flux
+using Random
+
 include("../algorithms/TwinDelayedDDPGfromDemos.jl")
 
-function create_actor(visual::Bool)
+function create_actor(visual::Bool, rng::AbstractRNG, state_size::Int, action_size::Int)
+    init=glorot_uniform(rng)
     if visual 
         Chain(
-            Conv((3, 3), 3 => 32, relu, stride=1, pad=0, init=glorot_uniform(rng), bias=false),
-            Conv((3, 3), 32 => 32, relu, stride=1, pad=0, init=glorot_uniform(rng), bias=false),
+            Conv((3, 3), 3 => 32, relu, stride=1, pad=0, init=init, bias=false),
+            Conv((3, 3), 32 => 32, relu, stride=1, pad=0, init=init, bias=false),
             MaxPool((4, 4)),
-            Conv((3, 3), 32 => 64, relu, stride=1, pad=0, init=glorot_uniform(rng), bias=false),
-            Conv((3, 3), 64 => 64, relu, stride=1, pad=0, init=glorot_uniform(rng), bias=false),
+            Conv((3, 3), 32 => 64, relu, stride=1, pad=0, init=init, bias=false),
+            Conv((3, 3), 64 => 64, relu, stride=1, pad=0, init=init, bias=false),
             MaxPool((4, 4)),
             Flux.flatten,
             Dense(256, 60, relu; init = init),
             Dense(60, 60, relu; init = init),
-            Dense(60, na, tanh; init = init),
+            Dense(60, action_size, tanh; init = init),
         ) |> gpu
     else
         Chain(
-            Dense(ns, 60, relu; init = init),
+            Dense(state_size, 60, relu; init = init),
             Dense(60, 60, relu; init = init),
-            Dense(60, na, tanh; init = init),
+            Dense(60, action_size, tanh; init = init),
         )|> gpu
     end
 end
 
-function create_critic_model(visual)
+function create_critic_model(visual::Bool, rng::AbstractRNG, state_size::Int, action_size::Int)
+    init=glorot_uniform(rng)
     if visual
         Chain(
             CombineActionImageEmbedding(Chain(
-            Conv((3, 3), 3 => 32, relu, stride=1, pad=0, init=glorot_uniform(rng), bias=false),
-            Conv((3, 3), 32 => 32, relu, stride=1, pad=0, init=glorot_uniform(rng), bias=false),
+            Conv((3, 3), 3 => 32, relu, stride=1, pad=0, init=init, bias=false),
+            Conv((3, 3), 32 => 32, relu, stride=1, pad=0, init=init, bias=false),
             MaxPool((4, 4)),
-            Conv((3, 3), 32 => 64, relu, stride=1, pad=0, init=glorot_uniform(rng), bias=false),
-            Conv((3, 3), 64 => 64, relu, stride=1, pad=0, init=glorot_uniform(rng), bias=false),
+            Conv((3, 3), 32 => 64, relu, stride=1, pad=0, init=init, bias=false),
+            Conv((3, 3), 64 => 64, relu, stride=1, pad=0, init=init, bias=false),
             MaxPool((4, 4)),
             Flux.flatten,
             ), vcat),
-            Dense(256 + na, 60, relu; init = init),
+            Dense(256 + action_size, 60, relu; init = init),
             Dense(60, 60, relu; init = init),
             Dense(60, 1; init = init),
         ) |> gpu
     else
         Chain(
-        Dense(ns + 1, 30, relu; init = init),
+        Dense(state_size + action_size, 30, relu; init = init),
         Dense(30, 30, relu; init = init),
         Dense(30, 1; init = init),
         )|> gpu
     end
 end
 
-create_critic(visual) = TwinDelayedDDPGCritic{visual}([create_critic_model(visual), create_critic_model(visual)])
+create_critic(visual::Bool, rng::AbstractRNG, state_size::Int, action_size::Int) = TwinDelayedDDPGCritic{visual}([create_critic_model(visual, rng, state_size, action_size), create_critic_model(visual, rng, state_size, action_size)])
--- src/scripts/train_student.jl
+++ src/scripts/train_student.jl
@@ -1,85 +1,112 @@
-using ReinforcementLearning
-using StableRNGs
-using Flux
-using BSON
-using IntervalSets
-using TensorBoardLogger
-using Logging  
-using Robosuite
-using CircularArrayBuffers
+include("../utilities/training_dependencies.jl")
 
-include("../utilities/CombinedTrajectory.jl")
-include("../utilities/hooks.jl")
-include("../utilities/utils.jl")
-include("../models/network_definitions.jl")
-include("../algorithms/TwinDelayedDDPGfromDemos.jl")
 
-image_size = 64;
-frame_size = 3;
-visual = true;
-run_name = "lift_test";
+function parse_commandline()
+    s = ArgParseSettings()
 
-rng = StableRNG(123);
-env = RoboticEnv(name="Lift", T=Float32, controller="OSC_POSE", enable_visual=visual, show=false, horizon=200, image_size=image_size)
+    @add_arg_table s begin
+        "--env"
+            help = "Environment name, e.g Lift or Door"
+            arg_type = String
+            default = "Lift"
+        "--run_name"
+            help = "Run name for logging purposes"
+            arg_type = String
+            required = true
+        "--pre_steps"
+            help = "Number of pretraining steps"
+            arg_type = Int
+            default = 20_000
+        "--repr_weight"
+            help = "Representation loss weight"
+            arg_type = Float32
+            default = 0.05f0
+        "--lambda"
+            help = "Relative weight for Q and BC losses"
+            arg_type = Float32
+            default = 2.5f0
+    end
 
-na = env.degrees_of_freedom;
+    return parse_args(s)
+end
 
-init = glorot_uniform(rng)
 
-BSON.@load "datasets/lift_demo.bson" dataset
-BSON.@load "agents/groundtruth/Lift" agent
-teacher = agent
+function main()
 
-demo_trajectory = dataset
+    parsed_args = parse_commandline()
+    env = parsed_args["env"]
+    pretraining_steps = parsed_args["pre_steps"]
+    representation_weight = parsed_args["repr_weight"]
+    q_bc_weight = parsed_args["lambda"]
+    run_name = parsed_args["run_name"]
 
-trajectory = CombinedTrajectory(CircularArraySGARTTrajectory(
-    capacity = 300_000,
-    state = Vector{Float32} => (image_size, image_size, frame_size),
-    groundtruth = Vector{Float32} => (size(vcat(vec(env.proprioception_state), vec(env.object_state)))[1]),
-    action = Vector{Float32} => (na,)
-), demo_trajectory, 0.25)
+    image_size = 64;
+    frame_size = 3;
+    visual = true;
 
-agent = Agent(
-    policy = TwinDelayedDDPGPolicy(
-        behavior_actor = NeuralNetworkApproximator(
-            model = create_actor(visual),
-            optimizer = ADAM(1e-4),
-        ),
-        behavior_critic = NeuralNetworkApproximator(
-            model = create_critic(visual),
-            optimizer = ADAM(1e-4),
-        ),
-        target_actor = NeuralNetworkApproximator(
-            model = create_actor(visual),
-            optimizer = ADAM(1e-4),
-        ),
-        target_critic = NeuralNetworkApproximator(
-            model = create_critic(visual),
-            optimizer = ADAM(1e-4),
-        ),
-        teacher = teacher |> gpu,
-        start_policy = RandomPolicy(Space([-1.0..1.0 for i=1:na]); rng = rng),
-        γ = 0.99f0,
-        ρ = 0.99f0,
-        batch_size = 256,
-        start_steps = 0,
-        pretraining_steps = 80_000,
-        update_freq = 1,
-        policy_freq = 2,
-        target_act_limit = 1.0,
-        target_act_noise = 0.1,
-        act_limit = 1.0,
-        act_noise = 0.1,
-        rng = rng,
-        q_bc_weight = 1.0f0,
-        critic_l2_weight = 1.0f-6,
-        actor_l2_weight = 1.0f-6,
-        representation_weight = 0.05f0,
-        ),
-    trajectory = trajectory
-);
+    rng = StableRNG(123);
+    env = RoboticEnv(name=env, T=Float32, controller="OSC_POSE", enable_visual=visual, show=false, horizon=200, image_size=image_size)
 
-stop_condition = StopAfterStep(300_000, is_show_progress=!haskey(ENV, "CI"));
-hook = tensorboard_hook(agent, string("logs/",run_name), save_checkpoints=true, agent_name=string("agents/visual/",run_name))
+    na = env.degrees_of_freedom;
+    ns = 42
 
-pretrain_run(agent, env, stop_condition, hook)
+    BSON.@load "datasets/lift_demo.bson" dataset
+    BSON.@load "agents/groundtruth/Lift" agent
+    teacher = agent
+
+    demo_trajectory = dataset
+
+    trajectory = CombinedTrajectory(CircularArraySGARTTrajectory(
+        capacity = 300_000,
+        state = Vector{Float32} => (image_size, image_size, frame_size),
+        groundtruth = Vector{Float32} => (size(vcat(vec(env.proprioception_state), vec(env.object_state)))[1]),
+        action = Vector{Float32} => (na,)
+    ), demo_trajectory, 0.25)
+
+    agent = Agent(
+        policy = TwinDelayedDDPGPolicy(
+            behavior_actor = NeuralNetworkApproximator(
+                model = create_actor(visual, rng, ns, na),
+                optimizer = ADAM(1e-4),
+            ),
+            behavior_critic = NeuralNetworkApproximator(
+                model = create_critic(visual, rng, ns, na),
+                optimizer = ADAM(1e-4),
+            ),
+            target_actor = NeuralNetworkApproximator(
+                model = create_actor(visual, rng, ns, na),
+                optimizer = ADAM(1e-4),
+            ),
+            target_critic = NeuralNetworkApproximator(
+                model = create_critic(visual, rng, ns, na),
+                optimizer = ADAM(1e-4),
+            ),
+            teacher = teacher |> gpu,
+            start_policy = RandomPolicy(Space([-1.0..1.0 for i=1:na]); rng = rng),
+            γ = 0.99f0,
+            ρ = 0.99f0,
+            batch_size = 256,
+            start_steps = 0,
+            pretraining_steps = pretraining_steps,
+            update_freq = 1,
+            policy_freq = 2,
+            target_act_limit = 1.0,
+            target_act_noise = 0.1,
+            act_limit = 1.0,
+            act_noise = 0.1,
+            rng = rng,
+            q_bc_weight = q_bc_weight,
+            critic_l2_weight = 1.0f-6,
+            actor_l2_weight = 1.0f-6,
+            representation_weight = representation_weight,
+            ),
+        trajectory = trajectory
+    );
+
+    stop_condition = StopAfterStep(10000, is_show_progress=!haskey(ENV, "CI"));
+    hook = tensorboard_hook(agent, string("logs/",run_name), save_checkpoints=true, agent_name=string("agents/visual/",run_name))
+
+    pretrain_run(agent, env, stop_condition, hook)
+end
+
+main()
