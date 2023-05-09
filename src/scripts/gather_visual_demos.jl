04fa9d721b6c6c7438946a94b4d5854a98054880
Romanos Voulgarakis <romanosv@aristotle5.it.auth.gr>
2023-05-09 11:31:35 +0300

    teacher training updated

--- src/scripts/gather_visual_demos.jl
+++ src/scripts/gather_visual_demos.jl
@@ -7,21 +7,22 @@ include("../utilities/utils.jl")
 
 image_size = 64
 
-env_name = "Door"
+env_name = "TwoArmPegInHole"
+robots = ("Panda", "Panda")
 
-env = RoboticEnv(name=env_name, T=Float32, controller="OSC_POSE", enable_visual=true, show=false, horizon=200, image_size=image_size, stop_when_done=true)
+env = RoboticEnv(name=env_name, robots=robots, T=Float32, controller="OSC_POSE", enable_visual=true, show=false, horizon=200, image_size=image_size, stop_when_done=true)
 
 na = env.degrees_of_freedom;
 
 BSON.@load string("agents/groundtruth/", env_name) agent
 
-stop_condition = StopAfterStep(10_000, is_show_progress=!haskey(ENV, "CI"));
+stop_condition = StopAfterStep(10000, is_show_progress=!haskey(ENV, "CI"));
 
 hook = SampleTrajectory{true}(CircularArraySGARTTrajectory(
     capacity = 30000,
     state = Vector{Float32} => (image_size,image_size,3,),
     groundtruth = Vector{Float32} => (size(vcat(vec(env.proprioception_state), vec(env.object_state)))[1],),
-    action = Vector{Float32} => (7,)
+    action = Vector{Float32} => (na,)
 ))
 
 actor_critic_agent = ActorCriticPolicy{false}(agent[:actor], agent[:critic]);
