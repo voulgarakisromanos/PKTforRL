a920f8ec40c9977c364b4967c64aea4bdf16df83
Romanos Voulgarakis <voulgarakisromanos@gmail.com>
2023-05-16 17:17:46 +0300

    Training with variable-strength teachers

--- src/scripts/train_student.jl
+++ src/scripts/train_student.jl
@@ -9,6 +9,10 @@ function parse_commandline()
             help = "Environment name, e.g Lift or Door"
             arg_type = String
             default = "Lift"
+        "--agent_name"
+            help = "Name of teacher agent"
+            arg_type = String
+            default = ""
         "--run_name"
             help = "Run name for logging purposes"
             arg_type = String
@@ -47,6 +51,7 @@ function main()
 
     parsed_args = parse_commandline()
     env_name = parsed_args["env"]
+    agent_name = parsed_args["agent_name"]
     pretraining_steps = parsed_args["pre_steps"]
     representation_weight = parsed_args["repr_weight"]
     q_bc_weight = parsed_args["lambda"]
@@ -82,7 +87,14 @@ function main()
     ns = size(vcat(vec(env.proprioception_state), vec(env.object_state)))[1]
 
     BSON.@load string("datasets/", env_name, "_demo.bson") dataset
-    BSON.@load string("agents/groundtruth/", env_name) agent
+
+    if agent_name == ""
+        BSON.@load string("agents/groundtruth/", env_name) agent
+        println(string("agents/groundtruth/", env_name))
+    else
+        BSON.@load string("agents/groundtruth/", agent_name) agent
+    end
+
     teacher = agent
 
     demo_trajectory = dataset
